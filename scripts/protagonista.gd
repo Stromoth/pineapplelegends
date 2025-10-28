d0d3bbd75deacd131d0e2a30d3414e6c78176dee
Stromoth <gabrielpaulocosta2020@gmail.com>
2025-10-28 19:28:29 -0300

    Novas animações

--- scripts/protagonista.gd
+++ scripts/protagonista.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 110.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -330.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +13,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true 
+	
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("walking")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:

543640df6a68347203966db0bd6789da016cbc74
Stromoth <gabrielpaulocosta2020@gmail.com>
2025-11-07 09:20:10 -0300

    Primeira versão do jogo completo

--- scripts/carro.gd
+++ scripts/carro.gd
@@ -1,35 +1,84 @@
 extends CharacterBody2D
 
-@onready var sensor = $Area2D
-var player_inside = false
-var player_ref = null
-var speed = 200.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -330.0
 
-func _ready():
-	sensor.body_entered.connect(_on_body_entered)
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var respawn_timer: Timer = $RespawnTimer
+@onready var start_position: Vector2 = global_position
 
-func _on_body_entered(body):
-	if body.name == "protagonista" and not player_inside:
-		player_ref = body
-		player_inside = true
-		player_ref.hide()
-		player_ref.set_physics_process(false)
-		print("Entrou no carro")
+var dead = false
+var in_car = false
 
-func _physics_process(_delta):
-	if player_inside:
-		var input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-		velocity.x = input * speed
-		move_and_slide()
+func _ready() -> void:
+	# conecta sinais por código (pode conectar no editor se preferir)
+	if respawn_timer:
+		respawn_timer.timeout.connect(_on_RespawnTimer_timeout)
+	if animated_sprite_2d:
+		# sinal chamado quando qualquer animação terminar
+		animated_sprite_2d.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
 
-		if Input.is_action_just_pressed("ui_accept"): # tecla E
-			sair_do_carro()
+func _physics_process(delta: float) -> void:
+	if dead:
+		return
 
-func sair_do_carro():
-	if player_ref:
-		player_ref.global_position = global_position + Vector2(50, 0)
-		player_ref.show()
-		player_ref.set_physics_process(true)
-		player_inside = false
-		player_ref = null
-		print("Saiu do carro")
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true 
+	
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("walking")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
+
+func die():
+	if dead:
+		return
+	dead = true
+	print("die() called")
+	animated_sprite_2d.play("death")
+	# quando a animação "death" terminar, o player continuará morto até o respawn_timer timeout
+	respawn_timer.start()
+
+func _on_RespawnTimer_timeout() -> void:
+	print("respawn timer timeout")
+	global_position = start_position
+	animated_sprite_2d.play("respawn")
+
+func _on_AnimatedSprite2D_animation_finished(anim_name: String) -> void:
+	# esse callback é chamado para qualquer animação; checamos se foi a de respawn
+	# dependendo da versão do Godot, o sinal pode passar o nome da animação ou não.
+	# por segurança verificamos a propriedade current animation também.
+	var current_anim := ""
+	if animated_sprite_2d.has_method("get_animation"):
+		current_anim = animated_sprite_2d.get_animation() # Godot 4?
+	else:
+		# Godot 3/compat fallback
+		if "animation" in animated_sprite_2d:
+			current_anim = animated_sprite_2d.animation
+	# se a versão do engine te passar o anim_name por parâmetro, priorize-o:
+	if anim_name != null and str(anim_name) != "":
+		current_anim = str(anim_name)
+
+	# debug
+	print("animation_finished:", current_anim)
+
+	if current_anim == "respawn":
+		dead = false
+		print("respawn finished -> freed controls")
